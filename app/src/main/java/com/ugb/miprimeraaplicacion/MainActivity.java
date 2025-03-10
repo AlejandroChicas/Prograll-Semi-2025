988256427e449e49ddf57138d453b218fd3ee813
josue chicas <josue.a.mejia@hotmail.com>
2025-03-10 13:47:21 -0600

    Continuamos con las operaciones del CRUD de amigos

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ugb.miprimeraaplicacion;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,8 +26,11 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import androidx.core.app.ActivityCompat;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
     Button btn;
     TextView tempVal;
     DB db;
@@ -41,7 +47,21 @@ public class MainActivity extends AppCompatActivity {
                 guardarAmigo();
             }
         });
+
+        db = new DB(this);
+        btn = findViewById(R.id.btnGuardarAmigo);
+        btn.setOnClickListener(view->guardarAmigo());
+
+        fab = findViewById(R.id.fabListaAmigos);
+        fab.setOnClickListener(view->abrirVentana());
+
     }
+
+    private void abrirVentana() {
+        Intent intent = new Intent(this, lista_amigos.class);
+        startActivity(intent);
+    }
+
     private void guardarAmigo() {
         tempVal = findViewById(R.id.txtNombre);
         String nombre = tempVal.getText().toString();
@@ -60,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
         String[] datos = {"", nombre, direccion, telefono, email, dui, ""};
         db.administrar_amigos("agregar", datos);
         Toast.makeText(getApplicationContext(), "Registro guardado con exito.", Toast.LENGTH_LONG).show();
+        abrirVentana();
     }
+
 
 }
