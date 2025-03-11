f835fffa6ea0756bda1f2abb87123542c908e36a
josue chicas <josue.a.mejia@hotmail.com>
2025-03-11 10:46:56 -0600

    Continuamos con el desarrollo de la agenda de amigos

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -3,6 +3,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.inputmethodservice.Keyboard;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.Editable;
@@ -29,24 +30,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONObject;
+
+import java.security.Key;
+
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
     Button btn;
     TextView tempVal;
     DB db;
+    String accion = "nuevo", idAmigo = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        db = new DB(this);
-        btn = findViewById(R.id.btnGuardarAmigo);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                guardarAmigo();
-            }
-        });
 
         db = new DB(this);
         btn = findViewById(R.id.btnGuardarAmigo);
@@ -55,13 +52,42 @@ public class MainActivity extends AppCompatActivity {
         fab = findViewById(R.id.fabListaAmigos);
         fab.setOnClickListener(view->abrirVentana());
 
+        mostrarDatos();
     }
+    private void mostrarDatos(){
+        try {
+            Bundle parametros = getIntent().getExtras();
+            accion = parametros.getString("accion");
+            if (accion.equals("modificar")) {
+                JSONObject datos = new JSONObject(parametros.getString("amigos"));
+                idAmigo = datos.getString("idAmigo");
 
-    private void abrirVentana() {
+                tempVal = findViewById(R.id.txtNombre);
+                tempVal.setText(datos.getString("nombre"));
+
+                tempVal = findViewById(R.id.txtDireccion);
+                tempVal.setText(datos.getString("direccion"));
+
+                tempVal = findViewById(R.id.txtTelefono);
+                tempVal.setText(datos.getString("telefono"));
+
+                tempVal = findViewById(R.id.txtEmail);
+                tempVal.setText(datos.getString("email"));
+
+                tempVal = findViewById(R.id.txtDui);
+                tempVal.setText(datos.getString("dui"));
+            }
+        }catch (Exception e){
+            mostrarMsg("Error: "+e.getMessage());
+        }
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    private void abrirVentana(){
         Intent intent = new Intent(this, lista_amigos.class);
         startActivity(intent);
     }
-
     private void guardarAmigo() {
         tempVal = findViewById(R.id.txtNombre);
         String nombre = tempVal.getText().toString();
@@ -82,6 +108,4 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "Registro guardado con exito.", Toast.LENGTH_LONG).show();
         abrirVentana();
     }
-
-
 }
