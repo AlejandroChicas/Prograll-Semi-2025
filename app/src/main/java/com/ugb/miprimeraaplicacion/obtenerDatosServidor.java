9f358680f4044f3a49e8ca1364e3c77b741f0522
josue chicas <josue.a.mejia@hotmail.com>
2025-04-30 12:02:38 -0600

    Tarea de tienda productos-CouchDB

--- app/src/main/java/com/ugb/miprimeraaplicacion/obtenerDatosServidor.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/obtenerDatosServidor.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.InputMismatchException;
 
 public class obtenerDatosServidor extends AsyncTask<String, String, String> {
     HttpURLConnection httpURLConnection;
