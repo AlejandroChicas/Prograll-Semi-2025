0ffd0ea590af49a98c154a74e7b6e56291710856
josue chicas <josue.a.mejia@hotmail.com>
2025-05-26 11:51:52 -0600

    Continuamos con el chats de amigos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     id("com.google.gms.google-services")
 }
-
 android {
     namespace = "com.ugb.miprimeraaplicacion"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.ugb.miprimeraaplicacion"
         minSdk = 23
@@ -34,10 +32,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-messaging")
